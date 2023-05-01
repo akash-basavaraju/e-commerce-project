7566f7bddcc1667b63d6630fed25885acb2bc615
Akash Basavaraju <bmnakash@gmail.com>
2023-05-01 09:11:06 +0530

    feat: browsing through service

--- src/pages/Browse.js
+++ src/pages/Browse.js
@@ -1,39 +1,46 @@
-import React from "react";
-import ProductImg from "../static/product_img.png";
+import React, { useState } from "react";
+import Service from "../shared/service";
 
-export default function Login({ usePage }) {
+export default function Login({ usePage, useCartProducts }) {
+  const [products] = useState(() => Service.getProducts());
+
   return (
     <div>
       <div
         style={{
-          margin: "10px",
-          cursor: "pointer",
           display: "flex",
+          justifyContent: "center",
+          fontSize: "18px",
+          fontWeight: "700",
         }}
       >
-        <img src={ProductImg} alt="product_img" width="100" />
-        <div style={{ margin: "13px", marginLeft: "25px" }}>
-          <div>Product 1</div>
-          <div>
-            product subject, product subject, product subject, product subject.
-          </div>
-        </div>
+        Available Products
       </div>
-      <div
-        style={{
-          margin: "10px",
-          cursor: "pointer",
-          display: "flex",
-        }}
-      >
-        <img src={ProductImg} alt="product_img" width="100" />
-        <div style={{ margin: "13px", marginLeft: "25px" }}>
-          <div>Product 1</div>
-          <div>
-            product subject, product subject, product subject, product subject.
+      {products.map(({ id, productImg, productName, productSubject }) => {
+        return (
+          <div
+            style={{
+              margin: "20px",
+              cursor: "pointer",
+              display: "flex",
+            }}
+            onClick={() => {
+              const index = useCartProducts[0].findIndex(
+                ({ id: fId }) => fId === id
+              );
+              if (index === -1) {
+                useCartProducts[1]([...useCartProducts[0], id]);
+              }
+            }}
+          >
+            <img src={productImg} alt="product_img" width="100" />
+            <div style={{ margin: "13px", marginLeft: "25px" }}>
+              <div>{productName}</div>
+              <div>{productSubject}</div>
+            </div>
           </div>
-        </div>
-      </div>
+        );
+      })}
     </div>
   );
 }
